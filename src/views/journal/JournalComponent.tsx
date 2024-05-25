715b1e4660a633370717afc86f1c5bb42163ccbc
domgan <ganczarski.dominik@gmail.com>
2024-05-25 11:28:41 +0200

    small change

--- src/views/journal/JournalComponent.tsx
+++ src/views/journal/JournalComponent.tsx
@@ -71,13 +71,11 @@ const JournalComponent = () => {
         <>
             <h1 className={style.header}>Journal</h1>
             <div className={style.questsWrapper}>
-                {!quests ? (
-                    <LoadingOverlay />
-                ) : (
+                {!quests ? <LoadingOverlay /> :
                     quests.map((quest) => (
                         <QuestItem key={quest.label} quest={quest} />
                     ))
-                )}
+                }
             </div>
         </>
     );
