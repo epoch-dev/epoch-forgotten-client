eb5fbe79ef10408f0e21170d11dc539a4f9c31c4
domgan <ganczarski.dominik@gmail.com>
2024-05-29 01:03:29 +0200

    update for buy endpoint

--- src/views/shop/ShopComponent.tsx
+++ src/views/shop/ShopComponent.tsx
@@ -1,15 +1,19 @@
 import style from './ShopComponent.module.scss';
-import { NpcShopItem } from "../../common/api/.generated";
+import { Item } from "../../common/api/.generated";
 import { AssetsService } from '../../common/services/AssetsService';
+import { ItemsClient } from '../../common/api/client';
+import { useGameStore } from '../game/GameStore';
+import { useState } from 'react';
+import { ToastService } from '../../common/services/ToastService';
 
-const ShopComponent = ({ npcShop, onClose }: { npcShop: NpcShopItem[]; onClose: () => void }) => {
+const ShopComponent = ({ npcShop, onClose }: { npcShop: Item[]; onClose: () => void }) => {
     return (
         <div className={style.shopViewOverlay}>
             <div className={style.shopView}>
                 <button className={style.closeShopButton} onClick={onClose}>X</button>
                 <div className={style.shopItems}>
                     {npcShop.map((shopItem, index) => (
-                        <ShopItem key={index} shopItem={shopItem} />
+                        <ShopItem key={index} item={shopItem} />
                     ))}
                 </div>
             </div>
@@ -17,22 +21,48 @@ const ShopComponent = ({ npcShop, onClose }: { npcShop: NpcShopItem[]; onClose: 
     );
 };
 
-const ShopItem = ({ shopItem }: { shopItem: NpcShopItem }) => {
-    return (
-        <div className={`${style.shopItem} ${style[shopItem.item.rarity]}`}>
+const ShopItem = ({ item }: { item: Item }) => {
+    const { npc } = useGameStore();
+    const [buying, setBuying] = useState(false);
+
+    const buyItem = async () => {
+        if (!npc) {
+            return;
+        }
+        setBuying(true);
+        try {
+            await ItemsClient.buyItem({ name: item.name, quantity: 1, npcName: npc.npcName });
+            ToastService.success({ message: 'Item bought' })
+        } catch (error) {
+            ToastService.error({ message: 'Error buying item' })
+        } finally {
+            setBuying(false);
+        }
+    }
+
+    return ( // todo: display and handle current money
+        <div className={`${style.shopItem} ${style[item.rarity]}`}>
             <div className={style.shopItemImageContainer}>
                 <img
                     className={style.shopItemImage}
-                    src={AssetsService.getItemUri(shopItem.item.imageUri)}
-                    alt={shopItem.item.name}
+                    src={AssetsService.getItemUri(item.imageUri)}
+                    alt={item.name}
                     draggable={false}
                 />
             </div>
             <div className={style.shopItemDetails}>
-                <h3>{shopItem.item.label}</h3>
-                <p>{shopItem.item.description}</p>
-                <p>Price: {shopItem.price}</p>
-                <button className={`btn ${style.buyButton}`}>Buy</button>
+                <h3>{item.label}</h3>
+                <p>{item.description}</p>
+                <p>Price: {item.price ?? 'Not availible'}</p>
+                {item.price &&
+                    <button
+                        className={`btn ${style.buyButton}`}
+                        onClick={buyItem}
+                        disabled={buying}
+                    >
+                        Buy
+                    </button>
+                }
             </div>
         </div>
     );
