f2f2a2735700b2c233c78ec1930b04c0a5dcf905
domgan <ganczarski.dominik@gmail.com>
2024-05-29 15:21:30 +0200

    after rebase changes

--- src/views/shop/ShopService.ts
+++ src/views/shop/ShopService.ts
@@ -1,12 +1,12 @@
-import { ItemsClient, UsersClient } from "../../common/api/client";
+import { itemsClient, usersClient } from "../../common/api/client";
 
 export class ShopService {
     public static async getGold() {
-        const user = (await UsersClient.whoami()).data;
+        const user = (await usersClient.whoami()).data;
         return user.gold;
     }
 
     public static async buyItem(itemName: string, npcName: string) {
-        await ItemsClient.buyItem({ name: itemName, quantity: 1, npcName });
+        await itemsClient.buyItem({ name: itemName, quantity: 1, npcName });
     }
 }
