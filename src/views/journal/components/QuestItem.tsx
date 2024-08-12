b7e2cd524ae1d612858b8425e281e1005e0fe806
domgan <ganczarski.dominik@gmail.com>
2024-08-12 20:48:04 +0200

    refactor quest item component

--- src/views/journal/components/QuestItem.tsx
+++ src/views/journal/components/QuestItem.tsx
@@ -1,26 +1,19 @@
 import style from '../JournalComponent.module.scss';
 import type { QuestsDtoInner } from '../../../common/api/.generated';
 import StageItem from './StageItem';
-import { useState } from 'react';
 
 const QuestItem = ({ quest }: { quest: QuestsDtoInner }) => {
-    const [expanded, setExpanded] = useState(false);
-
     return (
-        <div className={`${style.questItem} ${quest.main ? style.mainQuest : ''}`}>
-            <div>
-                <p onClick={() => setExpanded(!expanded)} className={`${style.subtitle} action dark`}>
-                    {expanded ? '▼' : '▶'} {quest.label}
-                </p>
+        <details className={`${style.questItem} ${quest.main ? style.mainQuest : ''}`}>
+            <summary className={`${style.subtitle} action dark`}>
+                {quest.label}
+            </summary>
+            <div className={style.stagesWrapper}>
+                {quest.stages.map((stage) => (
+                    <StageItem key={stage.id} stage={stage} unlocked={quest.unlocked} />
+                ))}
             </div>
-            {expanded && (
-                <div className={style.stagesWrapper}>
-                    {quest.stages.map((stage) => (
-                        <StageItem key={stage.id} stage={stage} unlocked={quest.unlocked} />
-                    ))}
-                </div>
-            )}
-        </div>
+        </details>
     );
 };
 
