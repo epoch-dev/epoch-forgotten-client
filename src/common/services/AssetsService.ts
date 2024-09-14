e4580a5e1a1092114e39028377c6fd9582baf5de
Dominik Ganczarski <domiran35@gmail.com>
2024-09-14 23:00:05 +0200

    test

--- src/common/services/AssetsService.ts
+++ src/common/services/AssetsService.ts
@@ -1,3 +1,4 @@
+const BASE_PATH = 'epoch-forgotten-client'
 const ICONS = {
     USER: 'user-icon.png',
     SKILLS: 'skills-icon.svg',
@@ -10,44 +11,44 @@ const ICONS = {
 type IconName = keyof typeof ICONS;
 
 export class AssetsService {
-    private static ICONS_BASE_URI = '/images/icons';
-    private static SCENES_BASE_URI = '/images/scenes';
-    private static MUSIC_BASE_URI = '/audio/music';
-    private static SOUNDS_BASE_URI = '/audio/sounds';
-    private static CHARACTERS_BASE_URI = '/images/characters';
-    private static ENEMIES_BASE_URI = '/images/enemies';
-    private static SKILLS_BASE_URI = '/images/skills';
-    private static ITEMS_BASE_URI = '/images/items';
+    private static ICONS_BASE_URI = 'images/icons';
+    private static SCENES_BASE_URI = 'images/scenes';
+    private static MUSIC_BASE_URI = 'audio/music';
+    private static SOUNDS_BASE_URI = 'audio/sounds';
+    private static CHARACTERS_BASE_URI = 'images/characters';
+    private static ENEMIES_BASE_URI = 'images/enemies';
+    private static SKILLS_BASE_URI = 'images/skills';
+    private static ITEMS_BASE_URI = 'images/items';
 
     public static getIcon(iconName: IconName) {
-        return `${this.ICONS_BASE_URI}/${ICONS[iconName]}`;
+        return `${BASE_PATH}/${this.ICONS_BASE_URI}/${ICONS[iconName]}`;
     }
 
     public static getSceneUri(sceneName: string) {
-        return `${this.SCENES_BASE_URI}/${sceneName}`;
+        return `${BASE_PATH}/${this.SCENES_BASE_URI}/${sceneName}`;
     }
 
     public static getMusicUri(musicName: string) {
-        return `${this.MUSIC_BASE_URI}/${musicName}`;
+        return `${BASE_PATH}/${this.MUSIC_BASE_URI}/${musicName}`;
     }
 
     public static getSoundUri(soundName: string) {
-        return `${this.SOUNDS_BASE_URI}/${soundName}`;
+        return `${BASE_PATH}/${this.SOUNDS_BASE_URI}/${soundName}`;
     }
 
     public static getCharacterUri(characterImageUri: string) {
-        return `${this.CHARACTERS_BASE_URI}/${characterImageUri}`;
+        return `${BASE_PATH}/${this.CHARACTERS_BASE_URI}/${characterImageUri}`;
     }
 
     public static getEnemyUri(enemyImageUri: string) {
-        return `${this.ENEMIES_BASE_URI}/${enemyImageUri}`;
+        return `${BASE_PATH}/${this.ENEMIES_BASE_URI}/${enemyImageUri}`;
     }
 
     public static getSkillUri(skillUri: string) {
-        return `${this.SKILLS_BASE_URI}/${skillUri}`;
+        return `${BASE_PATH}/${this.SKILLS_BASE_URI}/${skillUri}`;
     }
 
     public static getItemUri(itemUri: string) {
-        return `${this.ITEMS_BASE_URI}/${itemUri}`;
+        return `${BASE_PATH}/${this.ITEMS_BASE_URI}/${itemUri}`;
     }
 }
