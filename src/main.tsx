85cb65f2f0fd16aa33f13e1111b461958fd1ca8d
Dominik Ganczarski <domiran35@gmail.com>
2024-09-14 23:12:22 +0200

    Revert "test"
    This reverts commit 218eda69905495dd71bed7d3a9783cab5460825b.

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,10 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { AuthComponent } from './views/auth/AuthComponent';
 import { GameComponent } from './views/game/GameComponent';
 import { ToastContainer } from 'react-toastify';
-import { BASE_PATH } from './common/services/AssetsService.ts';
 
 const router = createBrowserRouter([
     {
-        path: `${BASE_PATH}/game`,
+        path: `game`,
         element: <GameComponent />,
     },
     {
