580fae93a176206041603470f49f2761c982d49b
Dominik Ganczarski <domiran35@gmail.com>
2024-09-15 15:17:51 +0200

    update

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,6 +1,5 @@
 export const appConfig = {
     apiUrl: import.meta.env.VITE_API_URL ?? 'http://localhost:3013',
     assetsPath: import.meta.env.VITE_ASSETS_PATH ?? './',
-    baseUrl: import.meta.env.BASE_URL ?? './',
     moveInterval: 100,
 };
