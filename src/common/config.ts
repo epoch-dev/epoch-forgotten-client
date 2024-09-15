919debbbfe1cd71fd89dce5e1b9441865cebc0a6
Dominik Ganczarski <domiran35@gmail.com>
2024-09-15 15:07:11 +0200

    enable deplyment to github pages

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,5 +1,6 @@
 export const appConfig = {
     apiUrl: import.meta.env.VITE_API_URL ?? 'http://localhost:3013',
     assetsPath: import.meta.env.VITE_ASSETS_PATH ?? './',
+    baseUrl: import.meta.env.BASE_URL ?? './',
     moveInterval: 100,
 };
