8ddd88c650561856d6163dc661797ff079a41259
domgan <domiran35@gmail.com>
2024-10-28 19:41:24 +0100

    display dialogue character images (#138)
    * display dialogue character images
    
    * improve css

--- src/views/dialogue/DialogueComponent.tsx
+++ src/views/dialogue/DialogueComponent.tsx
@@ -85,15 +85,27 @@ const DialogueComponent = () => {
                 currentNode && (
                     <div className={style.dialogueItem}>
                         {npcTitle && <div className={style.dialogueLabel}>{npcTitle}</div>}
-                        <p>
-                            {currentNode.author ? (
-                                <span>
-                                    <b>{currentNode.author}</b>: {currentNode.text}
-                                </span>
+                        <div className={style.dialogueContent}>
+                            {currentNode.imageUri ? (
+                                <img
+                                    src={AssetsService.getCharacterUri(currentNode.imageUri)}
+                                    alt={currentNode.author}
+                                    className={style.dialogueImage}
+                                />
                             ) : (
-                                <i>{currentNode.text}</i>
+                                <div className={style.dialogueImage} />
                             )}
-                        </p>
+                            <p>
+                                {currentNode.author ? (
+                                    <span>
+                                        <b>{currentNode.author}</b>: {currentNode.text}
+                                    </span>
+                                ) : (
+                                    <i>{currentNode.text}</i>
+                                )}
+                            </p>
+                            <div className={style.dialogueImage} />
+                        </div>
                         {currentNode.options && (
                             <>
                                 {currentNode.options.map((option, index) => (
