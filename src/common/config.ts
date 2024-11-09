05b5ed5b7610706ee3a4548b7d125bf6f7965c12
Dominik Ganczarski <domiran35@gmail.com>
2024-11-09 19:20:04 +0100

    add report issue button

--- src/common/config.ts
+++ src/common/config.ts
@@ -2,4 +2,5 @@ export const appConfig = {
     apiUrl: import.meta.env.VITE_API_URL ?? 'http://localhost:3013',
     assetsPath: import.meta.env.VITE_ASSETS_PATH ?? './',
     moveInterval: 200,
+    reportIssueLink: 'https://github.com/epoch-dev/epoch-forgotten-client/issues/new?assignees=&labels=bug&projects=&template=bug_report.md&title=',
 };
