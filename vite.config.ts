13fa84c660cabada5d9e30431fd96f93ea111e0d
domgan <domiran35@gmail.com>
2025-01-12 19:51:55 +0100

    deploy stage to github pages (#177)
    * deploy stage to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: '/epoch-forgotten-client/',
+export default defineConfig(({ mode }) => ({
+    base: `${mode === 'stage' ? '/stage' : ''}/epoch-forgotten-client/`,
     plugins: [react()],
     envDir: './',
-});
+}));
