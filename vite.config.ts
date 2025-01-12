9a478755dd19b13d5b7cfdb01d23d3acf24f9791
domgan <ganczarski.dominik@gmail.com>
2025-01-12 20:17:34 +0100

    Revert "stage deployment attempt"
    This reverts commit e99bd5f5e303c07666063677e1d33521154ee7d4.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: `/epoch-forgotten-client/${mode === 'stage' ? 'stage' : ''}`,
+    base: `${mode === 'stage' ? '/stage' : ''}/epoch-forgotten-client/`,
     plugins: [react()],
     envDir: './',
 }));
