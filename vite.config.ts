e99bd5f5e303c07666063677e1d33521154ee7d4
domgan <ganczarski.dominik@gmail.com>
2025-01-12 20:05:38 +0100

    stage deployment attempt

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: `${mode === 'stage' ? '/stage' : ''}/epoch-forgotten-client/`,
+    base: `/epoch-forgotten-client/${mode === 'stage' ? 'stage' : ''}`,
     plugins: [react()],
     envDir: './',
 }));
